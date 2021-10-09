36e32714871096ab152357c6294948ed572e86f4
Nuno Louro <nlouro@student.42heilbronn.de>
2021-10-09 11:29:22 +0200

    Added test file for reading

--- get_next_line.h
+++ get_next_line.h
@@ -6,11 +6,11 @@
 /*   By: nlouro <nlouro@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/08 16:34:20 by nlouro            #+#    #+#             */
-/*   Updated: 2021/10/09 11:00:24 by nlouro           ###   ########.fr       */
+/*   Updated: 2021/10/09 11:09:52 by nlouro           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
+#include <unistd.h>
 #include <stdlib.h>
 
 char	*get_next_line(int fd);
--- test/test_get_next_line.c
+++ test/test_get_next_line.c
@@ -1,10 +1,23 @@
 #include <stdio.h>
 #include "../get_next_line.c"
+#include <sys/stat.h>
+#include <fcntl.h>
 
 #define	BUFFER_SIZE 42
 
 int main()
 {
+	const char filepath[] = "test/file.txt";
+	int fd;
+	char *nl;
     printf("get_next_line called w/ BUFFER_SIZE = %d\n", BUFFER_SIZE);
+
+    printf("opening file: %s\n", filepath);
+	fd = open(filepath, O_RDONLY);
+    printf("file descriptor is: %d\n", fd);
+
+	nl = get_next_line(fd);
+    printf("file first line content: %s\n", nl); 
+
 	return (0);
 }
