54c49bb2d28f847a7cba7595c50c9e0179847a3f
Nuno <nuno@innoflair.com>
2021-11-02 10:53:08 +0100

    WIP - added test file for failing test...

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: nlouro <nlouro@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/08 16:35:31 by nlouro            #+#    #+#             */
-/*   Updated: 2021/10/31 14:41:24 by nlouro           ###   ########.fr       */
+/*   Updated: 2021/10/31 22:29:06 by nlouro           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 ssize_t	ft_nl_index(char *s)
 {
-	ssize_t	i;
+	int i;
 
 	i = 0;
 	while (s[i] != '\n' && s[i] != '\0')
@@ -31,38 +31,42 @@ char	*get_next_line(int fd)
 	static char *buffer;
 	char *nline;
 	ssize_t index;
-	ssize_t blen;
 	ssize_t tlen;
 
-	if (fd < 0 || fd > 100)
+	if (fd < 0 || fd > 100 || BUFFER_SIZE < 1)
 		return (NULL);
-	temp = (char *) malloc(sizeof(char));
 	if (buffer == NULL)
 		buffer = (char *) malloc(sizeof(char));
-	nline = (char *) malloc(sizeof(char));
 //	printf("DEBUG start buffer: %s\n", buffer);
 	index = -1;
-	blen = ft_strlen(buffer);
-	//printf("DEBUG n is: %zi\n", n);
 	if (ft_strchr(buffer, '\n') == NULL)
 	{
+		temp = (char *) malloc(sizeof(char));
 		tlen = 0;
 		while (ft_strchr(temp, '\n') == NULL)
 		{
 			tlen = read(fd, temp, BUFFER_SIZE);
 			buffer = ft_strjoin(buffer, temp);
-//			printf("DEBUG new buffer: %s\n", buffer);
-			if (tlen == 0)
+			//printf("DEBUG tlen: %zd\n", tlen);
+			//printf("DEBUG temp: %s\n", temp);
+			//printf("DEBUG new buffer: %s\n", buffer);
+			if (tlen <= 0)
 				break;
 		}
+		free(temp);
 	}
 	index = ft_nl_index(buffer);
 //	printf("DEBUG \\n found in pos: %zi\n", index);
-//	printf("DEBUG work buffer: %s\n", buffer);
 	if (index == -1)
+	{
+		free(buffer);
 		return (NULL);
-	nline = ft_substr(buffer, 0, index);
-	buffer = ft_substr(buffer, index + 1, 999);
-	//printf("DEBUG last buffer: %s\n", buffer);
-	return (nline);
+	}
+	else
+	{
+		nline = (char *) malloc(sizeof(char));
+		nline = ft_substr(buffer, 0, index);
+		buffer = ft_substr(buffer, index + 1, ft_strlen(buffer));
+		return (nline);
+	}
 }
