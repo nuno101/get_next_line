393def0a26b5928111304dda00c7f3f38739d2b0
Nuno Louro <nlouro@student.42heilbronn.de>
2021-11-02 15:40:37 +0100

    WIP - need to use malloc correctly

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: nlouro <nlouro@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/08 16:35:31 by nlouro            #+#    #+#             */
-/*   Updated: 2021/10/31 22:29:06 by nlouro           ###   ########.fr       */
+/*   Updated: 2021/11/02 15:05:47 by nlouro           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,10 +25,55 @@ ssize_t	ft_nl_index(char *s)
 		return (-1);
 }
 
+void    *ft_memcpy(void *dst, const void *src, size_t n)
+{
+    char    *ptr_d;
+    char    *ptr_s;
+
+    if (dst == (void *)0 && src == (void *)0)
+        return ((void *)0);
+    ptr_d = dst;
+    ptr_s = (char *)src;
+    while (n > 0)
+    {
+        *ptr_d = *ptr_s;
+        n--;
+        ptr_d++;
+        ptr_s++;
+    }
+    return (dst);
+}
+
+void	*ft_realloc(void *ptr, size_t olen, size_t nlen)
+{
+	void *nptr;
+
+	if (nlen == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	else if (ptr == NULL)
+		return malloc(nlen);
+	else if (nlen <= olen)
+		return (ptr);
+	else
+	{
+		nptr = malloc(nlen);
+		if (nptr)
+		{
+			ft_memcpy(nptr, ptr, olen);
+			free(ptr);
+		}
+		return (nptr);
+	}
+}
+
 char	*get_next_line(int fd)
 {
 	char *temp;
 	static char *buffer;
+	ssize_t blen;
 	char *nline;
 	ssize_t index;
 	ssize_t tlen;
@@ -36,27 +81,30 @@ char	*get_next_line(int fd)
 	if (fd < 0 || fd > 100 || BUFFER_SIZE < 1)
 		return (NULL);
 	if (buffer == NULL)
-		buffer = (char *) malloc(sizeof(char));
-//	printf("DEBUG start buffer: %s\n", buffer);
+		buffer = (char *) malloc((BUFFER_SIZE + 1) * sizeof(char));
+	else
+	{
+		blen = ft_strlen(buffer);
+		temp = (char *) malloc((blen + BUFFER_SIZE + 1) * sizeof(char));
+	}
+	//buffer = (char *) ft_realloc(buffer, blen, blen + BUFFER_SIZE);
 	index = -1;
 	if (ft_strchr(buffer, '\n') == NULL)
 	{
-		temp = (char *) malloc(sizeof(char));
+		temp = (char *) malloc(BUFFER_SIZE * sizeof(char));
 		tlen = 0;
 		while (ft_strchr(temp, '\n') == NULL)
 		{
 			tlen = read(fd, temp, BUFFER_SIZE);
 			buffer = ft_strjoin(buffer, temp);
-			//printf("DEBUG tlen: %zd\n", tlen);
 			//printf("DEBUG temp: %s\n", temp);
-			//printf("DEBUG new buffer: %s\n", buffer);
 			if (tlen <= 0)
 				break;
 		}
 		free(temp);
 	}
 	index = ft_nl_index(buffer);
-//	printf("DEBUG \\n found in pos: %zi\n", index);
+	//	printf("DEBUG \\n found in pos: %zi\n", index);
 	if (index == -1)
 	{
 		free(buffer);
